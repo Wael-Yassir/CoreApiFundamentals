bb8883ceddc374d9d60bf80c5c9ca485f45e9098
Wael-Yassir <wael.yassir@gmail.com>
2022-05-14 11:58:39 +0200

    Add Talk controller.

---
+++ src/Controllers/TalksContorller.cs
@@ -0,0 +1,91 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace CoreCodeCamp.Controllers
+{
+    [ApiController]
+    [Route("api/camps/{moniker}/talks")]
+    public class TalksContorller : ControllerBase
+    {
+        private readonly ICampRepository _campRepository;
+        private readonly IMapper _mapper;
+        private readonly LinkGenerator _linkGenerator;
+
+        public TalksContorller(ICampRepository campRepository, IMapper mapper, LinkGenerator linkGenerator)
+        {
+            this._campRepository = campRepository;
+            this._mapper = mapper;
+            this._linkGenerator = linkGenerator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<TalkModel[]>> Get(string moniker)
+        {
+            try
+            {
+                var talks = await _campRepository.GetTalksByMonikerAsync(moniker, true);
+                return _mapper.Map<TalkModel[]>(talks);
+            }
+            catch (System.Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<TalkModel>> Get(string moniker, int id)
+        {
+            try
+            {
+                var talk = await _campRepository.GetTalkByMonikerAsync(moniker, id, true);
+                return _mapper.Map<TalkModel>(talk);
+            }
+            catch (System.Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure!");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TalkModel>> Post(string moniker, TalkModel model)
+        {
+            try
+            {
+                var camp = await _campRepository.GetCampAsync(moniker);
+                if (camp == null) return BadRequest("Camp Does Not Exist!");
+
+                var talk = _mapper.Map<Talk>(model);
+                talk.Camp = camp;
+
+                if (model.Speaker == null) return BadRequest("Speaker ID is required!");
+                var speaker = await _campRepository.GetSpeakerAsync(model.Speaker.SpeakerId);
+                if (speaker == null) return BadRequest("Speaker could not be found!");
+                talk.Speaker = speaker;
+
+                _campRepository.Add(talk);
+
+                string location = _linkGenerator.GetPathByAction(HttpContext, "Get",
+                    values: new { moniker, id = model.TalkId });
+
+                if (await _campRepository.SaveChangesAsync())
+                {
+                    return Created(location, _mapper.Map<TalkModel>(talk));
+                }
+                else
+                {
+                    return BadRequest("Failure To Save New Talk!");
+                }
+
+            }
+            catch (System.Exception)
+            {
+                return BadRequest("Database Failure");
+            }
+        }
+    }
+}
---
+++ src/Data/Profiles/CampProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Models;
+
+namespace CoreCodeCamp.Data.Profiles
+{
+    public class CampProfile : Profile
+    {
+        public CampProfile()
+        {
+            this.CreateMap<Camp, CampModel>()
+                .ReverseMap();
+
+            this.CreateMap<Location, LocationModel>()
+                .ForMember(lm => lm.Venue, o => o.MapFrom(l => l.VenueName))
+                .ReverseMap();
+
+            this.CreateMap<Talk, TalkModel>()
+                .ReverseMap();
+
+            this.CreateMap<Speaker, SpeakerModel>()
+                .ReverseMap();
+        }
+    }
+}
