ed0fb2e1f2fcf472db388877915224516baf2bd6
Wael-Yassir <wael.yassir@gmail.com>
2022-05-14 15:49:05 +0200

    Add post, put, and delete actions for talk

--- src/Controllers/TalksContorller.cs
+++ src/Controllers/TalksContorller.cs
@@ -43,6 +43,12 @@ namespace CoreCodeCamp.Controllers
             try
             {
                 var talk = await _campRepository.GetTalkByMonikerAsync(moniker, id, true);
+
+                if (talk == null)
+                {
+                    return NotFound("The specified talk is not found!");
+                }
+
                 return _mapper.Map<TalkModel>(talk);
             }
             catch (System.Exception)
@@ -81,6 +87,68 @@ namespace CoreCodeCamp.Controllers
                     return BadRequest("Failure To Save New Talk!");
                 }
 
+            }
+            catch (System.Exception)
+            {
+                return BadRequest("Database Failure");
+            }
+        }
+
+        [HttpPut("{talkId:int}")]
+        public async Task<ActionResult<TalkModel>> Put(string moniker, int talkId, TalkModel model)
+        {
+            try
+            {
+                // check if there is a talk with the given data
+                var oldTalk = await _campRepository.GetTalkByMonikerAsync(moniker, talkId, true);
+                if (oldTalk == null) return NotFound("The specified talk does not exist!");
+
+                // map all the data form model to db entity
+                // camp and spealer should be mapping manually
+                _mapper.Map(model, oldTalk);
+
+                if (model.Speaker != null)
+                {
+                    var speaker = await _campRepository.GetSpeakerAsync(model.Speaker.SpeakerId);
+
+                    if (speaker != null)
+                    {
+                        oldTalk.Speaker = speaker;
+                    }
+                }
+
+                if (await _campRepository.SaveChangesAsync())
+                {
+                    return _mapper.Map<TalkModel>(oldTalk);
+                }
+                else
+                {
+                    return BadRequest("Failed to update database!");
+                }
+            }
+            catch (System.Exception)
+            {
+                return BadRequest("Database Failure");
+            }
+        }
+
+        [HttpDelete("{talkId:int}")]
+        public async Task<IActionResult> Delete(string moniker, int talkId)
+        {
+            try
+            {
+                var talk = await _campRepository.GetTalkByMonikerAsync(moniker, talkId);
+                if (talk == null) return NotFound("Failed to find talk to database!");
+
+                _campRepository.Delete(talk);
+                if (await _campRepository.SaveChangesAsync())
+                {
+                    return Ok("The talk deleted successfully!");
+                }
+                else
+                {
+                    return BadRequest("Failed to delete the talk!");
+                }
             }
             catch (System.Exception)
             {
--- src/Data/Profiles/CampProfile.cs
+++ src/Data/Profiles/CampProfile.cs
@@ -11,11 +11,15 @@ namespace CoreCodeCamp.Data.Profiles
                 .ReverseMap();
 
             this.CreateMap<Location, LocationModel>()
-                .ForMember(lm => lm.Venue, o => o.MapFrom(l => l.VenueName))
+                .ForMember(lm => lm.Venue, opt => opt.MapFrom(l => l.VenueName))
                 .ReverseMap();
 
             this.CreateMap<Talk, TalkModel>()
-                .ReverseMap();
+                .ReverseMap()
+                // when mapping form talkmodel to talk camp object is not define on talkmodel so,
+                // don't overrite it on talk and don't make it null (the same for speaker)
+                .ForMember(t => t.Camp, opt => opt.Ignore())
+                .ForMember(t => t.Speaker, opt => opt.Ignore());
 
             this.CreateMap<Speaker, SpeakerModel>()
                 .ReverseMap();
